76fad5185042b192452a9af377b37a05d451cf14
Starnaphie <41340960+Starnaphie@users.noreply.github.com>
2022-02-02 21:03:19 -0800

    fixed x2

--- someMethod.java
+++ someMethod.java
@@ -4,6 +4,7 @@ public class someMethod {
     public static int returnsNumberOfFailsIGetEachPA() {
         Random rand = new Random();
   
-        return rand.nextInt(100) + 100; 
+        // return rand.nextInt(100) + 100; 
+        return 50;
     }
 }

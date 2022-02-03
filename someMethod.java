6fa2f539a94f4c51b65539de390b11106d539f3a
Starnaphie <41340960+Starnaphie@users.noreply.github.com>
2022-02-02 21:01:59 -0800

    fixed

--- someMethod.java
+++ someMethod.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class someMethod {
-    public int returnsNumberOfFailsIGetEachPA() {
+    public static int returnsNumberOfFailsIGetEachPA() {
         Random rand = new Random();
   
         return rand.nextInt(100) + 100; 
--- someMethodTester.java
+++ someMethodTester.java
@@ -4,10 +4,10 @@ import org.junit.*;
 public class someMethodTester {
     @Test
     public void someMethodTest() {
-        assertEquals(50, returnsNumberOfFailsIGetEachPA()); // will fail
+        assertEquals(50, someMethod.returnsNumberOfFailsIGetEachPA()); // will fail
     }
 }
 
-// javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar someMethodTester.java
+// javac -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar someMethodTester.java
 
-// java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore someMethodTester
+// java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore someMethodTester
